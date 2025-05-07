a4b7267ee4d039e5bbc5843651cd5cdb5892c07d
MukterHossain <mukter3075@gmail.com>
2025-05-07 22:26:22 +0600

    setup typescript assignment

---
+++ solutions.ts
@@ -0,0 +1,44 @@
+
+{
+   //
+
+   // poblem 1
+   function formatString(input:string, toUpper?:boolean):string{
+        if(toUpper === true || toUpper === undefined){
+            return input.toUpperCase()
+        } else{
+            return input.toLowerCase()
+        }
+   }
+//    const result = formatString("hello")
+//    const result = formatString("hello", true)
+   const result = formatString("hello", false)
+
+   console.log(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+   
+
+
+
+
+
+
+
+
+
+
+   //
+}

ef09a03c133b464c715989a084355e207b3e1886
MukterHossain <mukter3075@gmail.com>
2025-05-08 17:31:19 +0600

    added five problem solved

--- solutions.ts
+++ solutions.ts
@@ -40,7 +40,121 @@
 
 
 
+ // problem 4
+ class Vehicle{
+    make:string;
+    year:number;
+    constructor(make:string, year:number){
+        this.make = make;
+        this.year = year;
+    }
+    
+   public getInfo():string{
+        return `Make: ${this.make}, Year: ${this.year}`
+    }
 
+ }
+
+ class Car extends Vehicle{
+    private model: string;
+    constructor(make:string, year:number, model:string){
+        super(make, year);
+        this.model = model;
+    }
+
+    public getModel():string{
+        return `Model: ${this.model}`;
+    }
+ }
+
+ const myCar = new Car("Toyota", 2020, "Corolla")
+ console.log(myCar.getInfo())
+ console.log(myCar.getModel())
+ 
+
+
+ // problem 5
+ function processValue(value:string |number):number{
+    if(typeof value === 'string'){
+        return value.length
+    }else{
+        return value * 2
+    }
+ }
+ 
+//  console.log(processValue("hello"))
+//  console.log(processValue(10))
+
+
+
+ // problem 6
+ interface Product{
+    name:string;
+    price:number;
+ }
+
+ function getMostExpensiveProduct(products:Product[]):Product| null{
+    if(products.length === 0){
+        return null
+    }
+    return products.reduce((prev, curr) => (prev.price > curr.price)? prev : curr)
+ }
+ const products= [
+    {name: "Pen", price: 10},
+    {name: "Notebook", price: 25},
+    {name: "Bag", price: 50},
+ ]
+
+//  console.log(getMostExpensiveProduct(products))
+
+
+ // problem 7
+ enum Day{
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday
+ }
+
+ function getDayType(day:Day):string{
+    switch(day){
+        case Day.Saturday:
+            case Day.Sunday:
+                return "Weekend"
+                default:
+                    return "Weekday"
+
+    }
+
+ }
+
+//  console.log(getDayType(Day.Monday))
+//  console.log(getDayType(Day.Sunday))
+//  console.log(getDayType(Day.Friday))
+//  console.log(getDayType(Day.Saturday))
+//  console.log(getDayType(Day.Wednesday))
+
+
+
+// probem 8
+
+async function squareAsync(n:number):Promise<number>{
+    return new Promise((resolve, reject) =>{
+        setTimeout(()=>{
+            if(typeof n === 'number' && n >=0){
+                resolve(n * n)
+            }
+            else{
+                reject(new Error("Error:Negative number not allowed"))
+            }
+        }, 1000)
+    })
+} 
+
+squareAsync(4).then((result => console.log(result)))
 
 
 
